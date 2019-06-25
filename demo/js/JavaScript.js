e3142a9a94391b30436071b40d0b7ab95fda19e9
mohammedbokhari <bookt19.15@gmail.com>
2019-06-25 13:37:34 +0300

    Addition of Education/Skill Separator line, Project Demos and Certifications, Videos, Images, and external links.

---
+++ demo/js/JavaScript.js
@@ -0,0 +1,15 @@
+function expr(){
+  $("html, body").animate({scrollTop : 825}, "fast");
+}
+
+function eduSkills(){
+  $("html, body").animate({scrollTop : 1920}, "fast");
+}
+
+function projDemoCerts(){
+  $("html, body").animate({scrollTop : $(document).height()-$(window).height()}, "fast");
+}
+
+function contact(){
+  $("html, body").animate({scrollTop : $("#name").offset().top}, "fast");
+}
