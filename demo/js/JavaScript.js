088e694b2139c06499b5bfa2bee8e1c9a4fc78c1
mohammedbokhari <bookt19.15@gmail.com>
2019-06-25 16:08:41 +0300

    Indentation fixes of HTML/CSS and documentation added.

--- demo/js/JavaScript.js
+++ demo/js/JavaScript.js
@@ -1,15 +1,16 @@
-function expr(){
+// SCROLL ANIMATION FUNCTIONS
+function expr(){ // EXPERIENCE
   $("html, body").animate({scrollTop : 825}, "fast");
 }
 
-function eduSkills(){
+function eduSkills(){ // EDUCATION/SKILLS
   $("html, body").animate({scrollTop : 1920}, "fast");
 }
 
-function projDemoCerts(){
+function projDemoCerts(){ // PROJECT DEMO/CERTIFICATIONS
   $("html, body").animate({scrollTop : $(document).height()-$(window).height()}, "fast");
 }
 
-function contact(){
+function contact(){ // CONTACT
   $("html, body").animate({scrollTop : $("#name").offset().top}, "fast");
 }
